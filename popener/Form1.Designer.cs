dc17045912bcc96aba2688c272b61f30be3c206f
komiy <komiy@HOME_PC>
2022-02-11 08:16:46 +0900

    プロジェクト ファイルを追加します。

---
+++ popener/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace popener
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.notifyIcon_enable = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenu_enable = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.disableToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.notifyIcon_disable = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenu_disable = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.enableToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.contextMenu_enable.SuspendLayout();
+            this.contextMenu_disable.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notifyIcon_enable
+            // 
+            this.notifyIcon_enable.ContextMenuStrip = this.contextMenu_enable;
+            this.notifyIcon_enable.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon_enable.Icon")));
+            this.notifyIcon_enable.Text = "notifyIcon1";
+            this.notifyIcon_enable.Visible = true;
+            // 
+            // contextMenu_enable
+            // 
+            this.contextMenu_enable.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenu_enable.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.disableToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.contextMenu_enable.Name = "contextMenu_enable";
+            this.contextMenu_enable.Size = new System.Drawing.Size(129, 52);
+            // 
+            // disableToolStripMenuItem
+            // 
+            this.disableToolStripMenuItem.Name = "disableToolStripMenuItem";
+            this.disableToolStripMenuItem.Size = new System.Drawing.Size(128, 24);
+            this.disableToolStripMenuItem.Text = "Disable";
+            this.disableToolStripMenuItem.Click += new System.EventHandler(this.disableToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(128, 24);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // notifyIcon_disable
+            // 
+            this.notifyIcon_disable.ContextMenuStrip = this.contextMenu_disable;
+            this.notifyIcon_disable.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon_disable.Icon")));
+            this.notifyIcon_disable.Text = "notifyIcon1";
+            // 
+            // contextMenu_disable
+            // 
+            this.contextMenu_disable.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenu_disable.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.enableToolStripMenuItem,
+            this.exitToolStripMenuItem1});
+            this.contextMenu_disable.Name = "contextMenu_disable";
+            this.contextMenu_disable.Size = new System.Drawing.Size(124, 52);
+            // 
+            // enableToolStripMenuItem
+            // 
+            this.enableToolStripMenuItem.Name = "enableToolStripMenuItem";
+            this.enableToolStripMenuItem.Size = new System.Drawing.Size(123, 24);
+            this.enableToolStripMenuItem.Text = "Enable";
+            this.enableToolStripMenuItem.Click += new System.EventHandler(this.enableToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem1
+            // 
+            this.exitToolStripMenuItem1.Name = "exitToolStripMenuItem1";
+            this.exitToolStripMenuItem1.Size = new System.Drawing.Size(123, 24);
+            this.exitToolStripMenuItem1.Text = "Exit";
+            this.exitToolStripMenuItem1.Click += new System.EventHandler(this.exitToolStripMenuItem1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(247, 75);
+            this.Enabled = false;
+            this.Name = "Form1";
+            this.Opacity = 0D;
+            this.ShowInTaskbar = false;
+            this.Text = "Form1";
+            this.TopMost = true;
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.contextMenu_enable.ResumeLayout(false);
+            this.contextMenu_disable.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon_enable;
+        private System.Windows.Forms.ContextMenuStrip contextMenu_enable;
+        private System.Windows.Forms.ToolStripMenuItem disableToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.NotifyIcon notifyIcon_disable;
+        private System.Windows.Forms.ContextMenuStrip contextMenu_disable;
+        private System.Windows.Forms.ToolStripMenuItem enableToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem1;
+        private System.Windows.Forms.ToolTip toolTip1;
+    }
+}
+
---
+++ popener/Form1.cs
@@ -0,0 +1,290 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+
+namespace popener
+{
+    public partial class Form1 : Form
+    {
+        KeyboardHook keyboard;
+        public Form1()
+        {
+            InitializeComponent();
+//            this.Hide();
+//            this.WindowState = FormWindowState.Minimized;
+            //Control.CheckForIllegalCrossThreadCalls = false;
+            keyboard = new KeyboardHook(this, notifyIcon_enable);
+        }
+
+        private void disableToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.notifyIcon_enable.Visible = false;
+            this.notifyIcon_disable.Visible = true;
+        }
+
+        private void enableToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.notifyIcon_disable.Visible = false;
+            this.notifyIcon_enable.Visible = true;
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.notifyIcon_enable.Visible = false;
+            this.notifyIcon_disable.Visible = false;
+            Application.Exit();
+        }
+
+        private void exitToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            this.notifyIcon_enable.Visible = false;
+            this.notifyIcon_disable.Visible = false;
+            Application.Exit();
+        }
+    }
+
+    interface IKeyState
+    {
+//        System.Timers.Timer timer { get; set; }
+
+        IKeyState CtrlPressed(KeyboardHook kbh);
+        IKeyState CtrlReleased(KeyboardHook kbh);
+        IKeyState CPressed(KeyboardHook kbh);
+        IKeyState CReleased(KeyboardHook kbh);
+    }
+
+    class Normal : IKeyState
+    {
+        public IKeyState CtrlPressed(KeyboardHook kbh) {
+            Console.WriteLine("C Pressed -> Ctrl Locked");
+            return new CtrlLocked();
+        }
+        public IKeyState CtrlReleased(KeyboardHook kbh) { return this; }
+        public IKeyState CPressed(KeyboardHook kbh) { return this; }
+        public IKeyState CReleased(KeyboardHook kbh) { return this; }
+    }
+
+    class CtrlLocked : IKeyState
+    {
+        public IKeyState CtrlPressed(KeyboardHook kbh) { return this; }
+        public IKeyState CtrlReleased(KeyboardHook kbh) 
+        {
+            Console.WriteLine("CtrlReleased -> Normal");
+            return new Normal();
+        }
+        public IKeyState CPressed(KeyboardHook kbh) {
+            Console.WriteLine("C Pressed -> Copied");
+            return new Copied();
+        }
+        public IKeyState CReleased(KeyboardHook kbh) { return this; }
+    }
+    
+    class Copied : IKeyState
+    {
+        public IKeyState CtrlPressed(KeyboardHook kbh) { return this; }
+        public IKeyState CtrlReleased(KeyboardHook kbh)
+        {
+            Console.WriteLine("Ctrl released -> Normal");
+            return new Normal();
+        }
+        public IKeyState CPressed(KeyboardHook kbh) { return this; }
+        public IKeyState CReleased(KeyboardHook kbh) {
+            Console.WriteLine("C Released -> Ready to open");
+            kbh.timer.Enabled = true;
+            return new ReadyToOpen();
+        }
+    }
+
+    class ReadyToOpen : IKeyState
+    {
+        public IKeyState CtrlPressed(KeyboardHook kbh) { return this; }
+        public IKeyState CtrlReleased(KeyboardHook kbh) {
+            Console.WriteLine("C Pressed -> Normal");
+            kbh.timer.Enabled = false;
+            return new Normal();
+        }
+        public IKeyState CPressed(KeyboardHook kbh)
+        {
+            kbh.timer.Enabled = false;
+            Console.WriteLine("C Pressed -> PathOpened");
+            IDataObject data = Clipboard.GetDataObject();
+            if (data != null)
+            {
+                if (data.GetDataPresent(typeof(string)))
+                {
+                    string textData = (string)data.GetData(DataFormats.Text);
+                    if (Regex.IsMatch(textData, @"^\\\\"))
+                    {
+                        string command = "/C explorer.exe " + textData;
+                        Process.Start("cmd.exe", command);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid Path");
+                        //Point p = new Point(Control.MousePosition.Y + 15, Control.MousePosition.X + 38);
+                        //kbh._form.Activate();
+                        //kbh.ToolTip1.Show("Invalid Path", kbh._form, 0, 0, 3000);
+                        //                        Help.ShowPopup(kbh._form, "Invalid Path", p);
+                        Point p = new Point(Control.MousePosition.X + 38, Control.MousePosition.Y + 15);
+                        //kbh._form.Activate();
+                        //Help.ShowPopup(kbh._form, "Invali Pathaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", p);
+                        kbh._icon.BalloonTipTitle = "Invalid Path";
+                        kbh._icon.BalloonTipText = (textData == "" ? "clipboard is empty" : textData);
+                        kbh._icon.BalloonTipIcon = ToolTipIcon.Info;
+                        kbh._icon.ShowBalloonTip(500);
+                    }
+                }
+            }
+            return new PathOpened();
+        }
+        public IKeyState CReleased(KeyboardHook kbh) { return this; }
+    }
+
+    class PathOpened : IKeyState
+    {
+        public IKeyState CtrlPressed(KeyboardHook kbh) { return this; }
+        public IKeyState CtrlReleased(KeyboardHook kbh)
+        {
+            Console.WriteLine("C Released -> Normal");
+            return new Normal();
+        }
+        public IKeyState CPressed(KeyboardHook kbh) { return this; }
+        public IKeyState CReleased(KeyboardHook kbh)
+        {
+            Console.WriteLine("C Released -> CtrlLocked");
+            return new CtrlLocked();
+        }
+    }
+
+    public class KeyboardHook
+    {
+        private const int MOUSEEVENTF_LEFTDOWN = 0x2;
+        private const int MOUSEEVENTF_LEFTUP = 0x4;
+
+        private HookHandler hookDelegate;
+        private IntPtr hook;
+        private IKeyState _keyState = new Normal();
+        public System.Timers.Timer timer;
+        public Form _form;
+        public NotifyIcon _icon;
+        public ToolTip ToolTip1 = new ToolTip();
+
+        IntPtr hMod;
+
+        public delegate int HookHandler(int nCode, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern IntPtr SetWindowsHookEx(int hookType, HookHandler hookDelegate, IntPtr module, uint threadId);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern bool UnhookWindowsHookEx(IntPtr hook);
+
+        [DllImport("user32.dll")]
+        public static extern int CallNextHookEx(IntPtr hook, int nCode, IntPtr wParam, IntPtr lParam);
+
+        #region Win32 Constants
+        protected const int WH_KEYBOARD_LL = 0x000D;
+        protected const int WM_KEYDOWN = 0x0100;
+        protected const int WM_KEYUP = 0x0101;
+        protected const int VK_LCONTROL = 0x00A2;
+        protected const int VK_RCONTROL = 0x00A3;
+        protected const int VK_C = 0x0043;
+        #endregion
+
+        #region Win32API Structures
+        [StructLayout(LayoutKind.Sequential)]
+        public class KBDLLHOOKSTRUCT
+        {
+            public uint vkCode;
+            public uint scanCode;
+            public KBDLLHOOKSTRUCTFlags flags;
+            public uint time;
+            public UIntPtr dwExtraInfo;
+        }
+
+        [Flags]
+        public enum KBDLLHOOKSTRUCTFlags : uint
+        {
+            KEYEVENTF_EXTENDEDKEY = 0x0001,
+            KEYEVENTF_KEYUP = 0x0002,
+            KEYEVENTF_SCANCODE = 0x0008,
+            KEYEVENTF_UNICODE = 0x0004,
+        }
+        #endregion
+
+        public KeyboardHook(Form form, NotifyIcon icon)
+        {
+            hookDelegate = new HookHandler(OnHook);
+            hMod = Marshal.GetHINSTANCE(System.Reflection.Assembly.GetExecutingAssembly().GetModules()[0]);
+            hook = SetWindowsHookEx(WH_KEYBOARD_LL, hookDelegate, hMod, 0);
+            _form = form;
+            _icon = icon;
+            ToolTip1.ShowAlways = true;
+            ToolTip1.IsBalloon = true;
+            if (hook == IntPtr.Zero)
+            {
+                int errorCode = Marshal.GetLastWin32Error();
+                throw new Win32Exception(errorCode);
+            }
+            timer = new System.Timers.Timer(2000);
+            timer.Elapsed += (sender, e) =>
+            {
+                timer.Enabled = false;
+                Console.WriteLine("Exp");
+                icon.BalloonTipTitle = "Info";
+                icon.BalloonTipText = "Timeout";
+                icon.BalloonTipIcon = ToolTipIcon.Info;
+                icon.ShowBalloonTip(500);
+                Point p = new Point(Control.MousePosition.X + 38, Control.MousePosition.Y + 15);
+                //form.Activate();
+                //Help.ShowPopup(form, "Timeout", p);
+            };
+            timer.Start();
+            timer.Enabled = false;
+        }
+
+        int OnHook(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            var kb = (KBDLLHOOKSTRUCT)Marshal.PtrToStructure(lParam, typeof(KBDLLHOOKSTRUCT));
+            var vkCode = (int)kb.vkCode;
+            switch (vkCode)
+            {
+                case VK_LCONTROL:
+                case VK_RCONTROL:
+                    if (wParam == (IntPtr)WM_KEYDOWN)
+                    {
+                        _keyState = _keyState.CtrlPressed(this);
+                    }
+                    else if (wParam == (IntPtr)WM_KEYUP)
+                    {
+                        _keyState = _keyState.CtrlReleased(this);
+
+                    }
+                    break;
+                case VK_C:
+                    if (wParam == (IntPtr)WM_KEYDOWN)
+                    {
+                        _keyState = _keyState.CPressed(this);
+                    }
+                    else if (wParam == (IntPtr)WM_KEYUP)
+                    {
+                        _keyState = _keyState.CReleased(this);
+
+                    }
+                    break;
+                default:
+                    break;
+            }
+            return CallNextHookEx(hook, nCode, wParam, lParam);
+        }
+    }
+}
